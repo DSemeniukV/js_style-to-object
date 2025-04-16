a678d41e142512ae9817cc7385b6698c86787cc0
Denys Semeniuk <serking3@gmail.com>
2025-04-16 17:00:46 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const object = {};
+
+  const result = sourceString
+    .split(';')
+    .filter(Boolean)
+    .map((item) => item.split(':'))
+    .filter((tips) => tips.length === 2 && tips[0] && tips[1])
+    .map((prop) => [prop[0].trim(), prop[1].trim()]);
+
+  result.forEach(([key, value]) => (object[key] = value));
+
+  return object;
 }
 
 module.exports = convertToObject;
